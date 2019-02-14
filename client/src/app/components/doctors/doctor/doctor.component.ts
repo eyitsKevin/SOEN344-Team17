0070bb3ce64221aa87752b1e989b30c5e7de1581
Tristan Vu <phistan007@gmail.com>
2019-02-14 01:30:43 -0500

    [#17] - display doctor profile

--- client/src/app/components/doctors/doctor/doctor.component.ts
+++ client/src/app/components/doctors/doctor/doctor.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Doctor} from '../DoctorModel';
 @Component({
   selector: 'app-doctor',
   templateUrl: './doctor.component.html',
   styleUrls: ['./doctor.component.scss']
 })
+
 export class DoctorComponent implements OnInit {
 
+  doctor: Doctor = {
+    name : "Tristan Vu",
+    permit_number: "23451234",
+    specialty: "Pediatrician",
+    city : "Montreal",
+    clinic : "CLSC"
+  }
   constructor() { }
 
   ngOnInit() {
