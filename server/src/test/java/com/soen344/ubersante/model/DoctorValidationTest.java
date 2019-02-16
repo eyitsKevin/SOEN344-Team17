705154bd31a09b75608f357e618ab3642f8bb5a4
MJ <mjackson74u@gmail.com>
2019-02-16 15:15:30 -0500

    [#11] - added spring annotation to doctor and patient unit tests

--- server/src/test/java/com/soen344/ubersante/model/DoctorValidationTest.java
+++ server/src/test/java/com/soen344/ubersante/model/DoctorValidationTest.java
@@ -5,6 +5,9 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
@@ -15,6 +18,8 @@ import java.util.Set;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class DoctorValidationTest {
 
     private static ValidatorFactory validatorFactory;
