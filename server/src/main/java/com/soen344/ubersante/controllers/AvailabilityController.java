809e82893b1a339e56a7a64d62bfaf0f2b7a515a
Zhen-Yee <davster2@gmail.com>
2019-02-25 18:31:24 -0500

    [#43] - add basic mapping in AvailabilityController to call availabilityService

--- server/src/main/java/com/soen344/ubersante/controllers/AvailabilityController.java
+++ server/src/main/java/com/soen344/ubersante/controllers/AvailabilityController.java
@@ -3,11 +3,16 @@ package com.soen344.ubersante.controllers;
 import com.soen344.ubersante.dto.DoctorLoginForm;
 import com.soen344.ubersante.exceptions.DoctorNotFoundException;
 import com.soen344.ubersante.exceptions.InvalidPasswordException;
+import com.soen344.ubersante.models.Availability;
+import com.soen344.ubersante.services.AvailabilityService;
 import com.soen344.ubersante.services.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.validation.Valid;
 
@@ -15,5 +20,17 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/availability")
 public class AvailabilityController {
+
+    @Autowired
+    private AvailabilityService availabilityService;
+
+    @GetMapping("/view")
+    public ResponseEntity getAvailabilityByMonth(@RequestBody LocalDateTime date) {
+        try {
+            return new ResponseEntity<>(availabilityService.getAvailabilityByMonth(date), HttpStatus.OK);
+        } catch(Exception e) {
+            return new ResponseEntity<>("Server Error.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
     
 }
--- server/src/main/java/com/soen344/ubersante/services/AvailabilityService.java
+++ server/src/main/java/com/soen344/ubersante/services/AvailabilityService.java
@@ -1,10 +1,15 @@
 package com.soen344.ubersante.services;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import com.soen344.ubersante.dto.DoctorDetails;
 import com.soen344.ubersante.dto.DoctorLoginForm;
 import com.soen344.ubersante.exceptions.DoctorNotFoundException;
 import com.soen344.ubersante.exceptions.InvalidPasswordException;
+import com.soen344.ubersante.models.Availability;
 import com.soen344.ubersante.models.Doctor;
+import com.soen344.ubersante.repositories.AvailabilityRepository;
 import com.soen344.ubersante.repositories.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,4 +17,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class AvailabilityService {
 
+    @Autowired
+    AvailabilityRepository availabilityRepository;
+
+    public List<Availability> getAvailabilityByMonth(LocalDateTime date) {
+        return availabilityRepository.findByAllByMonth(date);
+    }
 }
