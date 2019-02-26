07d032b225944b437dbfe9a6a4ee5bd87b0cad1a
Zhen-Yee <davster2@gmail.com>
2019-02-26 04:15:19 -0500

    [#43] - add retrieval functions to test connection between FE and BE for availabilities

--- client/src/app/components/shared/sidenav/sidenav.component.ts
+++ client/src/app/components/shared/sidenav/sidenav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../../services/authentication.service';
 import { Router } from '@angular/router';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-sidenav',
@@ -11,7 +12,7 @@ export class SidenavComponent implements OnInit {
 
   authenticated;
 
-  constructor(private authenticationService: AuthenticationService, private router: Router) { }
+  constructor(private authenticationService: AuthenticationService, private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
     this.authenticationService.authenticated.subscribe(authenticated => this.authenticated = authenticated);
@@ -24,4 +25,13 @@ export class SidenavComponent implements OnInit {
     this.router.navigate(['/login']);
   }
 
+  retrieveWalkin() {
+    this.http.get("http://localhost:8080/availability/view/walkin/3")
+      .subscribe(result => {console.log(result)});
+  }
+
+  retrieveAnnual() {
+    this.http.get("http://localhost:8080/availability/view/annual/2")
+      .subscribe(result => {console.log(result)});
+  }
 }
