077dd75e99981429f3fdabc59603b351b192d28f
Kevin Luu <kevinateur@hotmail.com>
2019-03-01 00:44:52 -0500

    task/[#45] - Change frontend naming to match backend

--- client/src/app/components/patients/patient-cart/patient-cart/patient-cart.component.ts
+++ client/src/app/components/patients/patient-cart/patient-cart/patient-cart.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CartDataService } from '../../../../services/cart-data.service';
 import { AuthenticationService } from '../../../../services/authentication.service';
-import { HttpClient } from "@angular/common/http";
-import { MatSnackBar } from "@angular/material";
-import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-patient-cart',
@@ -13,42 +12,41 @@ import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 export class PatientCartComponent implements OnInit {
 
   constructor(private cartDataService: CartDataService,
-    private authenticationService: AuthenticationService,
-    public snackBar: MatSnackBar,
-    private http: HttpClient) { }
-list = []
-user;
-healthcard;
+              private authenticationService: AuthenticationService,
+              public snackBar: MatSnackBar,
+              private http: HttpClient) { }
+  cart = [];
+  patient;
+  healthcard;
   ngOnInit() {
-    this.list = this.cartDataService.getAllAppointments();
+    this.cart = this.cartDataService.getAllAppointments();
 
-    for(let i = 0; i<this.list.length; i++){
-     const fullDate =  this.list[i].startTime.split("T");
-     const fullDate2 =  this.list[i].endTime.split("T");
-      this.list[i].day = fullDate[0];
-      this.list[i].start = fullDate[1];
-      this.list[i].end = fullDate2[1];
+    for (let i = 0; i < this.cart.length; i++) {
+      const fullDate =  this.cart[i].startTime.split('T');
+      const fullDate2 =  this.cart[i].endTime.split('T');
+      this.cart[i].day = fullDate[0];
+      this.cart[i].start = fullDate[1];
+      this.cart[i].end = fullDate2[1];
     }
   }
 
-  remove(index){
+  remove(index) {
     this.cartDataService.removeAppointment(index);
   }
 
-  checkout(){
-    this.authenticationService.user.subscribe(user => this.user = user);
-    this.healthcard = this.user.healthCard;
-    const userWithList = {
-      user: this.healthcard,
-      list: this.list
-    }
-    this.http.post("http://localhost:8080/availability/cart/checkout", userWithList)
-        .subscribe(data => {
-          
+  checkout() {
+    this.authenticationService.user.subscribe(patient => this.patient = patient);
+    const patientWithcart = {
+      patient: this.patient,
+      cart: this.cart
+    };
+    this.http.post('/api/availability/cart/checkout', patientWithcart)
+      .subscribe(data => {
+
         },
-          error => { console.log(error); this.openSnackBar(error.error, "Close"); }
-        );
-    this.list = this.cartDataService.deleteAllAppointments();
+        error => { console.log(error); this.openSnackBar(error.error, 'Close'); }
+      );
+    this.cart = this.cartDataService.deleteAllAppointments();
   }
   openSnackBar(message: string, action: string) {
     this.snackBar.open(message, action, {
