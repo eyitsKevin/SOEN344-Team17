c0b585dc0a1022856232305fa5dcdbd80cfab269
Kevin Luu <kevinateur@hotmail.com>
2019-03-05 22:23:25 -0500

    task/[#45] - Add patient model

---
+++ client/src/app/models/patient.ts
@@ -0,0 +1,10 @@
+export class Patient {
+  healthCard: string;
+  firstName: string;
+  lastName: string;
+  birthday: string;
+  gender: string;
+  phone: string;
+  email: string;
+  address: string;
+}
