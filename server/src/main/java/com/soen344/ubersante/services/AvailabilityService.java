8de61a241c82d61e8f7bcc098883fd6764b46f70
MJ <mjackson74u@gmail.com>
2019-03-10 14:53:10 -0400

    [#58] - removed unused imports

--- server/src/main/java/com/soen344/ubersante/services/AvailabilityService.java
+++ server/src/main/java/com/soen344/ubersante/services/AvailabilityService.java
@@ -1,9 +1,7 @@
 package com.soen344.ubersante.services;
 
 import java.time.Duration;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
