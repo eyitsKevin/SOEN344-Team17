effc180f81d9618af01f6376f0ec9f79b7bbade1
MJ <mjackson74u@gmail.com>
2019-03-12 11:32:36 -0400

    [#78] - Clinic

---
+++ server/src/main/java/com/soen344/ubersante/models/Clinic.java
@@ -0,0 +1,95 @@
+package com.soen344.ubersante.models;
+
+import com.soen344.ubersante.validation.ValidClinicHours;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import java.util.Objects;
+
+@Entity
+@Table(name = "clinic")
+public class Clinic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name")
+    @NotBlank
+    private String name;
+
+    @Column(name = "rooms")
+    @Positive
+    private int rooms;
+
+    @Embedded
+    @ValidClinicHours
+    private ClinicHours clinicHours;
+
+    public Clinic() {}
+
+    public Clinic(String name, int rooms, ClinicHours clinicHours) {
+        this.name = name;
+        this.rooms = rooms;
+        this.clinicHours = clinicHours;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRooms() {
+        return rooms;
+    }
+
+    public ClinicHours getClinicHours() {
+        return clinicHours;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRooms(int rooms) {
+        this.rooms = rooms;
+    }
+
+    public void setClinicHours(ClinicHours clinicHours) {
+        this.clinicHours = clinicHours;
+    }
+
+    @Override
+    public String toString() {
+        return "Clinic{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", rooms=" + rooms +
+                ", clinicHours=" + clinicHours +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Clinic)) return false;
+        Clinic clinic = (Clinic) o;
+        return id == clinic.id &&
+                rooms == clinic.rooms &&
+                name.equals(clinic.name) &&
+                clinicHours.equals(clinic.clinicHours);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, rooms, clinicHours);
+    }
+}
---
+++ server/src/test/java/com/soen344/ubersante/model/ClinicValidationTest.java
@@ -0,0 +1,90 @@
+package com.soen344.ubersante.model;
+
+import com.soen344.ubersante.models.Availability;
+import com.soen344.ubersante.models.Clinic;
+import com.soen344.ubersante.models.ClinicHours;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Set;
+
+public class ClinicValidationTest {
+
+    private static ValidatorFactory validatorFactory;
+    private static Validator validator;
+    private Clinic clinic;
+
+    @BeforeClass
+    public static void createValidator() {
+        validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+    }
+
+    @AfterClass
+    public static void close() {
+        validatorFactory.close();
+    }
+
+    @Before
+    public void setUp() {
+        clinic = new Clinic();
+        clinic.setId(1);
+        clinic.setName("name");
+        clinic.setRooms(5);
+        clinic.setClinicHours(new ClinicHours(LocalTime.of(8, 0), LocalTime.of(8, 20)));
+    }
+
+    @Test
+    public void shouldHaveNoViolations() {
+
+        Set<ConstraintViolation<Clinic>> violations = validator.validate(clinic);
+
+        assertTrue("No Violations for Clinic", violations.isEmpty());
+    }
+
+    @Test
+    public void shouldDetectBlankName() {
+        clinic.setName("");
+
+        Set<ConstraintViolation<Clinic>> violations = validator.validate(clinic);
+        assertEquals(1, violations.size());
+
+        for (ConstraintViolation<Clinic> violation : violations) {
+            assertEquals("must not be blank", violation.getMessage());
+        }
+    }
+
+    @Test
+    public void shouldDetectNegativeRoom() {
+        clinic.setRooms(0);
+
+        Set<ConstraintViolation<Clinic>> violations = validator.validate(clinic);
+        assertEquals(1, violations.size());
+
+        for (ConstraintViolation<Clinic> violation : violations) {
+            assertEquals("must be greater than 0", violation.getMessage());
+        }
+    }
+
+    @Test
+    public void shouldDetectInvalidClinicHours() {
+        clinic.setClinicHours(new ClinicHours(LocalTime.of(8, 20), LocalTime.of(8, 0)));
+
+        Set<ConstraintViolation<Clinic>> violations = validator.validate(clinic);
+        assertEquals(1, violations.size());
+
+        for (ConstraintViolation<Clinic> violation : violations) {
+            assertEquals("invalid clinic hours", violation.getMessage());
+        }
+    }
+}
