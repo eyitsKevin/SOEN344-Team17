70d802a49500e8fb89e511c5913df5f6daf82add
Tamar Merdkhanian <tamar.merdkhanian@gmail.com>
2019-03-12 15:20:36 -0400

    added doctor upcoming appointments landing page

--- client/src/app/components/doctors/upcoming-appointments/upcoming-appointments.component.ts
+++ client/src/app/components/doctors/upcoming-appointments/upcoming-appointments.component.ts
@@ -12,7 +12,7 @@ export class UpcomingAppointmentsComponent implements OnInit {
   appointments = [];
   user;
 
-  constructor(private authenticationService: AuthenticationService, private http: HttpClient) {}
+  constructor(private authenticationService: AuthenticationService, private http: HttpClient) { }
 
   ngOnInit() {
     this.authenticationService.user.subscribe(user => this.user = user);
@@ -20,6 +20,38 @@ export class UpcomingAppointmentsComponent implements OnInit {
   }
 
   getAppointments(permit) {
+    this.http.get("http://localhost:8080/appointment/doctor/" + permit).subscribe(data => {
+      for (var i in data) {
+        let appointment = {
+          date: data[i]["date"],
+          time: data[i]["time"],
+          appointmentType: data[i]["appointmentType"]=="WALK_IN" ? "Walk-In" : "Check up",
+          patient: data[i]["patientDetails"].firstName + " " + data[i]["patientDetails"].lastName
+        }
+        this.appointments.push(appointment);
+      }
+
+      function comparebyDate(a, b) {
+        if (a.date < b.date)
+          return -1;
+        if (a.date > b.date)
+          return 1;
+        return 0;
+      }
+  
+      function comparebyTime(a, b) {
+        if (a.time < b.time)
+          return -1;
+        if (a.time > b.time)
+          return 1;
+        return 0;
+      }
+  
+      this.appointments.sort(comparebyTime);
+      this.appointments.sort(comparebyDate);
+
+    });
+
   }
 
   convertTime(time) {
