843af33ea49fd370d3b94e32a1299613472959d0
Zhen-Yee <davster2@gmail.com>
2019-03-13 22:55:46 -0400

    add cartService to store patient cart in memory

---
+++ server/src/main/java/com/soen344/ubersante/services/CartService.java
@@ -0,0 +1,35 @@
+package com.soen344.ubersante.services;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import com.soen344.ubersante.dto.AvailabilityDto;
+import com.soen344.ubersante.enums.AppointmentType;
+import com.soen344.ubersante.exceptions.*;
+import com.soen344.ubersante.dto.AvailabilityDetails;
+import com.soen344.ubersante.dto.PatientDetails;
+import com.soen344.ubersante.models.Appointment;
+import com.soen344.ubersante.models.Availability;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import javax.validation.*;
+
+import java.sql.Timestamp;
+
+@Service
+public class CartService {
+
+    private HashMap<String, List<Availability>> patientsCart = new HashMap();
+
+    public void saveAvailability(String healthCard, List<Availability> availabilities) {
+        patientsCart.put(healthCard, availabilities);
+    }
+}
