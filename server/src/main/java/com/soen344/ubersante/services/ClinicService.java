b05fb2b6d45a8a1ee5c4eee01d22e1ee6722b879
Zhen-Yee <davster2@gmail.com>
2019-03-28 18:17:59 -0400

    add base ClinicService

---
+++ server/src/main/java/com/soen344/ubersante/services/ClinicService.java
@@ -0,0 +1,11 @@
+package com.soen344.ubersante.services;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClinicService {
+
+}
