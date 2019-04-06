4a7c247d50dc73b346973699ae6740d09a1d382e
phistanx <phistan007@gmail.com>
2019-04-06 13:18:23 -0400

    added validateLogin to interface so it can be used with proxy

--- server/src/main/java/com/soen344/ubersante/services/IPatientService.java
+++ server/src/main/java/com/soen344/ubersante/services/IPatientService.java
@@ -3,8 +3,13 @@ package com.soen344.ubersante.services;
 import com.soen344.ubersante.dto.PatientRegistrationForm;
 import com.soen344.ubersante.exceptions.PatientAlreadyExistsException;
 import com.soen344.ubersante.models.Patient;
+import com.soen344.ubersante.dto.PatientDetails;
+import com.soen344.ubersante.dto.PatientLoginForm;
+import com.soen344.ubersante.exceptions.PatientNotFoundException;
+import com.soen344.ubersante.exceptions.InvalidPasswordException;
 
 public interface IPatientService {
 
     Patient registerNewPatient(PatientRegistrationForm patientRegistrationForm) throws PatientAlreadyExistsException;
+    PatientDetails validateLogin(PatientLoginForm loginForm) throws PatientNotFoundException, InvalidPasswordException;
 }
