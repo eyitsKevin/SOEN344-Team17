256e7d5200ced80096eed050ac298921c3dc9ee6
TangMichael <tangmichael6@hotmail.com>
2019-04-10 16:11:41 -0400

    flyweight

---
+++ server/src/main/java/com/soen344/ubersante/models/AvailabilitiesByMonth.java
@@ -0,0 +1,10 @@
+package com.soen344.ubersante.models;
+
+import java.util.List;
+
+public class AvailabilitiesByMonth {
+
+    public List<Availability> listAvail;
+    public boolean hasChanged = true;
+
+}
---
+++ server/src/main/java/com/soen344/ubersante/models/ClinicAvailabilities.java
@@ -0,0 +1,8 @@
+package com.soen344.ubersante.models;
+
+public class ClinicAvailabilities {
+
+    public AvailabilitiesByMonth[] annual = new AvailabilitiesByMonth[12];
+    public AvailabilitiesByMonth[] walkin = new AvailabilitiesByMonth[12];
+    
+}
