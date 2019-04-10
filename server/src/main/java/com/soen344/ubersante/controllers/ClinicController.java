4ab659c4a0b1fae93aed8b04cc7a890c1b87d341
Kevin Luu <kevinateur@hotmail.com>
2019-04-10 18:30:24 -0400

    Clinic getting doctors and nurses

--- server/src/main/java/com/soen344/ubersante/controllers/ClinicController.java
+++ server/src/main/java/com/soen344/ubersante/controllers/ClinicController.java
@@ -4,10 +4,7 @@ import com.soen344.ubersante.services.ClinicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
@@ -23,4 +20,15 @@ public class ClinicController {
         return new ResponseEntity<>(clinicService.getAllClinics(), HttpStatus.OK);
         
     }
+
+    @GetMapping("/view/doctors/{id}")
+    public ResponseEntity viewDoctorsByClinic(@PathVariable long id) {
+        return new ResponseEntity<>(clinicService.getDoctorByClinicId(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/view/nurses/{id}")
+    public ResponseEntity viewNursesByClinic(@PathVariable long id) {
+        return new ResponseEntity<>(clinicService.getNurseByClinicId(id), HttpStatus.OK);
+    }
+
 }
--- server/src/main/java/com/soen344/ubersante/services/ClinicService.java
+++ server/src/main/java/com/soen344/ubersante/services/ClinicService.java
@@ -1,7 +1,11 @@
 package com.soen344.ubersante.services;
 
 import com.soen344.ubersante.models.Clinic;
+import com.soen344.ubersante.models.Doctor;
+import com.soen344.ubersante.models.Nurse;
 import com.soen344.ubersante.repositories.ClinicRepository;
+import com.soen344.ubersante.repositories.DoctorRepository;
+import com.soen344.ubersante.repositories.NurseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,11 +13,31 @@ import java.util.List;
 
 @Service
 public class ClinicService {
-    
+
     @Autowired
     private ClinicRepository clinicRepository;
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    @Autowired
+    private NurseRepository nurseRepository;
 
     public List<Clinic> getAllClinics() {
         return clinicRepository.findAll();
     }
+
+    public Clinic getClinicById(long id) {
+        return clinicRepository.getOne(id);
+    }
+
+    public List<Doctor> getDoctorByClinicId(long id) {
+        return doctorRepository.findAllByClinicId(id);
+    }
+
+    public List<Nurse> getNurseByClinicId(long id) {
+        return nurseRepository.findAllByClinicId(id);
+    }
+
+
 }
