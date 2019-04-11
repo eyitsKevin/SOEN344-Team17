7f1f13488358a85862bec5bc2e95f82ccfa80573
wuwentang <wuwen_tang@hotmail.com>
2019-04-10 20:05:19 -0400

    clinic profile display

--- client/src/app/components/clinics/clinic-profile/clinic-profile.component.ts
+++ client/src/app/components/clinics/clinic-profile/clinic-profile.component.ts
@@ -1,39 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {NestedTreeControl} from '@angular/cdk/tree';
-import {MatTreeNestedDataSource} from '@angular/material/tree';
-
-interface FoodNode {
-  name: string;
-  children?: FoodNode[];
-}
-
-const TREE_DATA: FoodNode[] = [
-  {
-    name: 'Fruit',
-    children: [
-      {name: 'Apple'},
-      {name: 'Banana'},
-      {name: 'Fruit loops'},
-    ]
-  }, {
-    name: 'Vegetables',
-    children: [
-      {
-        name: 'Green',
-        children: [
-          {name: 'Broccoli'},
-          {name: 'Brussel sprouts'},
-        ]
-      }, {
-        name: 'Orange',
-        children: [
-          {name: 'Pumpkins'},
-          {name: 'Carrots'},
-        ]
-      },
-    ]
-  },
-];
 
 @Component({
   selector: 'app-clinic-profile',
@@ -42,14 +7,7 @@ const TREE_DATA: FoodNode[] = [
 })
 export class ClinicProfileComponent implements OnInit {
 
-  treeControl = new NestedTreeControl<FoodNode>(node => node.children);
-  dataSource = new MatTreeNestedDataSource<FoodNode>();
-
-  constructor() {
-    this.dataSource.data = TREE_DATA;
-  }
-
-  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
+  constructor() {}
 
   ngOnInit() {
   }
